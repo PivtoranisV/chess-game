a7cb858f800243d7f50c900bf7ccca051176426e
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-16 10:56:04 +0300

    Add test for validating starting position of Rock

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,9 +6,15 @@ describe Board do
   subject(:board) { described_class.new }
 
   describe '#initialize' do
-    it 'Creates an empty board with 8 columns and 8 rows' do
-      expect(board.grid.length).to eq(8)
-      expect(board.grid.all? { |row| row.length == 8 }).to be true
+    context 'When board initialize' do
+      it 'Creates a board with 8 columns and 8 rows' do
+        expect(board.grid.length).to eq(8)
+        expect(board.grid.all? { |row| row.length == 8 }).to be true
+      end
+
+      it 'Place Rock in the first column of first row' do
+        expect(board.grid[0][0]).to eq("\u2656")
+      end
     end
   end
 end
