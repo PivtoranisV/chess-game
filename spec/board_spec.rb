b49b9cd9b0bfa4b4caf0d76dc3bc42fe2fcaffa2
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-16 11:34:48 +0300

    Add tests for validating starting position of pieces

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,8 +12,23 @@ describe Board do
         expect(board.grid.all? { |row| row.length == 8 }).to be true
       end
 
-      it 'Place Rock in the first column of first row' do
+      it 'Place Black Rock in the first column of first row' do
         expect(board.grid[0][0]).to eq("\u265C")
+      end
+      it 'Place Black Knight in the second column of first row' do
+        expect(board.grid[0][1]).to eq("\u265E")
+      end
+      it 'Place Black King in the fifth column of first row' do
+        expect(board.grid[0][4]).to eq("\u265A")
+      end
+      it 'Place White King in the fifth column of last row' do
+        expect(board.grid[7][4]).to eq("\u2654")
+      end
+      it 'Place White Knight in the second column of last row' do
+        expect(board.grid[7][1]).to eq("\u2658")
+      end
+      it 'Place White pawns in the each column of second row' do
+        expect(board.grid[1].all? { |cell| cell == "\u265F" }).to be true
       end
     end
   end
