ecbdc2ddd0bcc0e5827c1c36802dc56d04bbb738
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-17 14:47:01 +0300

    Fixed Bishop possible moves

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -7,14 +7,17 @@ class Bishop < Pieces
     super(position, color == :black ? "\u265D" : "\u2657", color)
   end
 
-  def possible_moves
+  def possible_moves(board)
     position_x, position_y = position
     moves = []
-    7.downto(1) { |move| moves << [position_x + move, position_y + move] }
-    7.downto(1) { |move| moves << [position_x - move, position_y - move] }
-    7.downto(1) { |move| moves << [position_x + move, position_y - move] }
-    7.downto(1) { |move| moves << [position_x - move, position_y + move] }
-    # Return moves only within the 8x8 board
-    valid_moves(moves)
+    directions = [[1, 1], [-1, -1], [1, -1], [-1, 1]]
+
+    directions.each do |direction|
+      (1..7).each do |move|
+        new_pos = [position_x + move * direction[0], position_y + move * direction[1]]
+        break if !valid_move?(new_pos) || add_moves(moves, new_pos, board)
+      end
+    end
+    moves
   end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,28 +1,44 @@
 # frozen_string_literal: true
 
 require_relative '../lib/pieces/bishop'
+require_relative '../lib/board'
 
 describe Bishop do
+  let(:board) { instance_double(Board) }
+
+  before do
+    allow(board).to receive(:square_occupied?).and_return(nil)
+  end
+
   context 'When Bishop is at position [0, 0]' do
-    subject(:bishop01) { described_class.new([0, 0], :black) }
+    subject(:bishop00) { described_class.new([0, 0], :black) }
+
     it 'should return valid possible moves' do
       expected_moves = [[7, 7], [6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]
-      expect(bishop01.possible_moves).to match_array(expected_moves)
+      expect(bishop00.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'When Bishop is at position [7, 0]' do
-    subject(:bishop70) { described_class.new([7, 0], :black) }
+  context 'When Bishop is blocked by its own pieces' do
+    subject(:bishop70) { described_class.new([7, 0], :white) }
     it 'should return valid possible moves' do
-      expected_moves = [[0, 7], [1, 6], [2, 5], [3, 4], [4, 3], [5, 2], [6, 1]]
-      expect(bishop70.possible_moves).to match_array(expected_moves)
+      allow(board).to receive(:square_occupied?).with([4, 3]).and_return(double('Piece', color: :white))
+      allow(board).to receive(:square_occupied?).with([5, 2]).and_return(nil)
+      allow(board).to receive(:square_occupied?).with([6, 1]).and_return(nil)
+
+      expected_moves = [[5, 2], [6, 1]]
+      expect(bishop70.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'When Bishop is at position [4, 4]' do
+  context 'When Bishop captures an opponent piece' do
     subject(:bishop44) { described_class.new([4, 4], :black) }
     it 'should return valid possible moves' do
-      expected_moves = [[7, 7], [6, 6], [5, 5], [0, 0], [1, 1], [2, 2], [3, 3], [7, 1], [6, 2],
+      allow(board).to receive(:square_occupied?).with([7, 7]).and_return(nil)
+      allow(board).to receive(:square_occupied?).with([5, 3]).and_return(double('Piece', color: :white))
+      allow(board).to receive(:square_occupied?).with([3, 5]).and_return(nil)
+
+      expected_moves = [[7, 7], [6, 6], [5, 5], [0, 0], [1, 1], [2, 2], [3, 3],
                         [5, 3], [1, 7], [2, 6], [3, 5]]
-      expect(bishop44.possible_moves).to match_array(expected_moves)
+      expect(bishop44.possible_moves(board)).to match_array(expected_moves)
     end
   end
 end
