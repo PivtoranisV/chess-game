9bd3cf8eeee82c82eafe1c280029c5f8a9e1fa58
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-17 14:57:39 +0300

    Fixed Queen possible_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -7,21 +7,17 @@ class Queen < Pieces
     super(position, color == :black ? "\u265B" : "\u2655", color)
   end
 
-  def possible_moves
+  def possible_moves(board)
     position_x, position_y = position
     moves = []
-    # Horizontal moves
-    7.downto(1) { |move| moves << [position_x + move, position_y] }
-    7.downto(1) { |move| moves << [position_x - move, position_y] }
-    7.downto(1) { |move| moves << [position_x, position_y + move] }
-    7.downto(1) { |move| moves << [position_x, position_y - move] }
-    # Vertical moves
-    7.downto(1) { |move| moves << [position_x + move, position_y + move] }
-    7.downto(1) { |move| moves << [position_x - move, position_y - move] }
-    7.downto(1) { |move| moves << [position_x + move, position_y - move] }
-    7.downto(1) { |move| moves << [position_x - move, position_y + move] }
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1], [1, -1], [-1, 1]]
 
-    # Return moves only within the 8x8 board
-    valid_moves(moves)
+    directions.each do |direction|
+      (1..7).each do |move|
+        new_pos = [position_x + move * direction[0], position_y + move * direction[1]]
+        break if !valid_move?(new_pos) || add_moves(moves, new_pos, board)
+      end
+    end
+    moves
   end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -3,6 +3,12 @@
 require_relative '../lib/pieces/queen'
 
 describe Queen do
+  let(:board) { instance_double(Board) }
+
+  before do
+    allow(board).to receive(:square_occupied?).and_return(nil)
+  end
+
   context 'when Queen is at position [0, 0]' do
     subject(:queen00) { described_class.new([0, 0], :black) }
     it 'returns all valid moves' do
@@ -11,30 +17,36 @@ describe Queen do
         [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7],
         [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7]
       ]
-      expect(queen00.possible_moves).to match_array(expected_moves)
+      expect(queen00.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'when Queen is at position [4, 4]' do
+  context 'when Queen is blocked by its own pieces' do
     subject(:queen44) { described_class.new([4, 4], :black) }
     it 'returns all valid moves' do
+      allow(board).to receive(:square_occupied?).with([4, 3]).and_return(double('Piece', color: :black))
+      allow(board).to receive(:square_occupied?).with([5, 4]).and_return(nil)
+      allow(board).to receive(:square_occupied?).with([1, 7]).and_return(nil)
+
       expected_moves = [
-        [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4],
-        [4, 5], [4, 6], [4, 7], [4, 3], [4, 2], [4, 1], [4, 0],
-        [5, 5], [6, 6], [7, 7], [3, 3], [2, 2], [1, 1], [0, 0],
-        [5, 3], [6, 2], [7, 1], [3, 5], [2, 6], [1, 7]
+        [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4], [4, 5],
+        [4, 6], [4, 7], [5, 5], [6, 6], [7, 7], [3, 3], [2, 2], [1, 1],
+        [0, 0], [5, 3], [6, 2], [7, 1], [3, 5], [2, 6], [1, 7]
       ]
-      expect(queen44.possible_moves).to match_array(expected_moves)
+      expect(queen44.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'when Queen is at position [7, 3]' do
+  context 'when Queen captures an opponent piece' do
     subject(:queen73) { described_class.new([7, 3], :black) }
     it 'returns all valid moves' do
+      allow(board).to receive(:square_occupied?).with([6, 3]).and_return(nil)
+      allow(board).to receive(:square_occupied?).with([6, 3]).and_return(double('Piece', color: :white))
+      allow(board).to receive(:square_occupied?).with([4, 0]).and_return(nil)
+
       expected_moves = [
-        [6, 3], [5, 3], [4, 3], [3, 3], [2, 3], [1, 3], [0, 3],
-        [7, 4], [7, 5], [7, 6], [7, 7], [7, 2], [7, 1], [7, 0],
+        [6, 3], [7, 4], [7, 5], [7, 6], [7, 7], [7, 2], [7, 1], [7, 0],
         [6, 4], [5, 5], [4, 6], [3, 7], [6, 2], [5, 1], [4, 0]
       ]
-      expect(queen73.possible_moves).to match_array(expected_moves)
+      expect(queen73.possible_moves(board)).to match_array(expected_moves)
     end
   end
 end
