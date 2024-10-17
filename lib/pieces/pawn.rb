f0dde4a95e7490cb0147c9fbe1baa9341514a07d
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-17 15:37:38 +0300

    Fixed Pawn possible moves

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,7 +7,7 @@ class Pawn < Pieces
     super(position, color == :black ? "\u265F" : "\u2659", color)
   end
 
-  def possible_moves
+  def possible_moves(board)
     start_position_x, start_position_y = position
     moves = []
 
@@ -16,15 +16,29 @@ class Pawn < Pieces
 
     # Regular move (one square forward)
     regular_move = [start_position_x + direction, start_position_y]
-    moves << regular_move
+    moves << regular_move if valid_move?(regular_move) && !board.square_occupied?(regular_move)
 
     # First move (two squares forward)
     if (color == :white && start_position_x == 6) || (color == :black && start_position_x == 1)
+      one_step_forward = [start_position_x + direction, start_position_y]
       first_move = [start_position_x + 2 * direction, start_position_y]
-      moves << first_move
+
+      # Check that both one step and two steps forward are not blocked
+      if valid_move?(first_move) && !board.square_occupied?(one_step_forward) && !board.square_occupied?(first_move)
+        moves << first_move
+      end
     end
 
-    # Return moves only within the 8x8 board
-    valid_moves(moves)
+    # Capture moves
+    capture_moves = [
+      [start_position_x + direction, start_position_y + 1],
+      [start_position_x + direction, start_position_y - 1]
+    ]
+    capture_moves.each do |capture_move|
+      piece = board.square_occupied?(capture_move)
+      moves << capture_move if valid_move?(capture_move) && piece && piece.color != color
+    end
+
+    moves
   end
 end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,27 +1,43 @@
 # frozen_string_literal: true
 
 require_relative '../lib/pieces/pawn'
+require_relative '../lib/board'
 
 describe Pawn do
+  let(:board) { instance_double(Board) }
+
+  before do
+    allow(board).to receive(:square_occupied?).and_return(nil)
+  end
+
   context 'When White Pawn is at position [6, 3]' do
     subject(:pawn63) { described_class.new([6, 3], :white) }
     it 'should return valid possible moves' do
       expected_moves = [[5, 3], [4, 3]]
-      expect(pawn63.possible_moves).to match_array(expected_moves)
+      expect(pawn63.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'When Black Pawn is at position [1, 4]' do
+  context 'When Black Pawn is not at starting position' do
+    subject(:pawn20) { described_class.new([2, 0], :black) }
+    it 'should return valid possible move' do
+      expected_moves = [[3, 0]]
+      expect(pawn20.possible_moves(board)).to match_array(expected_moves)
+    end
+  end
+  context 'When Black Pawn is blocked by other piece' do
     subject(:pawn14) { described_class.new([1, 4], :black) }
     it 'should return valid possible moves' do
-      expected_moves = [[2, 4], [3, 4]]
-      expect(pawn14.possible_moves).to match_array(expected_moves)
+      allow(board).to receive(:square_occupied?).with([2, 4]).and_return(double('Piece', color: :white))
+      expected_moves = []
+      expect(pawn14.possible_moves(board)).to match_array(expected_moves)
     end
   end
-  context 'When White Pawn is at position [4, 3]' do
+  context 'When White Pawn is capturing' do
     subject(:pawn43) { described_class.new([4, 3], :white) }
-    it 'should return valid possible moves' do
-      expected_moves = [[3, 3]]
-      expect(pawn43.possible_moves).to match_array(expected_moves)
+    it 'should return valid possible moves for capturing' do
+      allow(board).to receive(:square_occupied?).with([3, 2]).and_return(double('Piece', color: :black))
+      expected_moves = [[3, 2], [3, 3]]
+      expect(pawn43.possible_moves(board)).to match_array(expected_moves)
     end
   end
 end
