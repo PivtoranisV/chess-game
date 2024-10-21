b77cae7cfd14543fddf068c4bd0cd0cac6e8dcb8
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-21 19:51:02 +0300

    Improve visual board appearance

--- lib/game.rb
+++ lib/game.rb
@@ -75,25 +75,26 @@ class Game
   end
 
   def display_board
-    puts '    |  a  |  b  |  c  |  d  |  e  |  f  |  g  |  h  |'
-    puts '----|-----|-----|-----|-----|-----|-----|-----|-----|----'
+    puts '    a   b   c   d   e   f   g   h '
+    puts '  +---+---+---+---+---+---+---+---+'
 
     @board.grid.reverse.each_with_index do |row, row_index|
       formatted_row = row.map.with_index do |cell, col_index|
         cell_display = cell.nil? ? '   ' : " #{colorize_piece(cell)} "
-        background_color = (row_index + col_index).even? ? :light_yellow : :light_blue
+        background_color = (row_index + col_index).even? ? :green : :light_blue
         cell_display.colorize(background: background_color)
-      end.join(' | ')
+      end.join('|')
 
-      puts " #{8 - row_index}  | #{formatted_row} |  #{8 - row_index} "
-      puts '____|_____|_____|_____|_____|_____|_____|_____|_____|____'
+      puts "#{8 - row_index} |#{formatted_row}| #{8 - row_index}"
+      puts '  +---+---+---+---+---+---+---+---+'
     end
-    puts '    |  a  |  b  |  c  |  d  |  e  |  f  |  g  |  h  |'
+
+    puts '     a   b   c   d   e   f   g   h '
   end
 
   def colorize_piece(piece)
-    piece_color = piece.color == :black ? :black : :white
-    piece.symbol.colorize(color: piece_color)
+    piece_color = piece.color == :black ? :black : :light_white
+    piece.symbol.colorize(color: piece_color, mode: :bold)
   end
 
   def create_player(default_name, other_color)
